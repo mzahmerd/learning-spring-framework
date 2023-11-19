819c5744118fb21afd7c755e3e2ca3f402aef39c
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2023-11-19 22:56:31 +0100

    field injection with @Autowired on dependency

--- src/main/java/com/mzahmerd/learnspring/game/GamingAppLauncherApplication.java
+++ src/main/java/com/mzahmerd/learnspring/game/GamingAppLauncherApplication.java
@@ -1,16 +1,14 @@
-package com.mzahmerd.learnspring;
+package com.mzahmerd.learnspring.game;
 
-import com.mzahmerd.learnspring.game.GameRunner;
-import com.mzahmerd.learnspring.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.mzahmerd.learnspring.game")
-public class GameLauncherApp {
+public class GamingAppLauncherApplication {
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext(GameLauncherApp.class)) {
+        try (var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
 
             context.getBean(GamingConsole.class).up();
 
