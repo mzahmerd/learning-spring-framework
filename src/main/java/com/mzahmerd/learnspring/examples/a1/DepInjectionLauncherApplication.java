f9ba46d7864829afd97ea865e67a046a98792cc1
Muhammad Zubairu <muhdzubairuahmad@gmail.com>
2023-11-19 23:04:26 +0100

    using constructor injection

--- src/main/java/com/mzahmerd/learnspring/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/mzahmerd/learnspring/examples/a1/DepInjectionLauncherApplication.java
@@ -10,21 +10,30 @@ import java.util.Arrays;
 
 @Component
 class YourBusinessClass{
+    @Autowired  //this is not compulsory when using constructor injection, and it's recommended injection type
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        System.out.println(("Constructor injection - Your business class"));git add .
+
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
     Dependency1 dependency1;
     Dependency2 dependency2;
 
 
-    @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        System.out.println("Setter injection - setDependency1");
-        this.dependency1 = dependency1;
-    }
-
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
-        System.out.println("Setter injection - setDependency2");
-        this.dependency2 = dependency2;
-    }
+//    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter injection - setDependency1");
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Setter injection - setDependency2");
+//        this.dependency2 = dependency2;
+//    }
 
 
 
